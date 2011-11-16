e7a5ffcf13fed685642cb78e337ee56a05616d2a
Vince Matonis <innatepirate@gmail.com>
2011-11-16 08:55:37 -0700

    adding shadows

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<body>
 		<div id="Wrapper">
 			<div id="CurTrack">
+				Now Playing<br />
 				<span id='TrackText'>&nbsp;</span>
 			</div>
 			<a href="#" onclick="ButtonPress('prev');return true;">
@@ -19,6 +20,9 @@
 			<a href="#" onclick="ButtonPress('next');return true;">
 				<img class="btn" src="img/next.png" />
 			</a>
+		</div>
+		<div id="Footer">
+			Copyright &copy; Vince Matonis 2011 (just for the hell of it, you know?)
 		</div>
 		<div id="Floater">
 			
